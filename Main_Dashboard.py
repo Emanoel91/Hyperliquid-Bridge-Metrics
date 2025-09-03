3b4989f6b6bba5be2e4f1a607ef0ecc39c7cb7e4
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-03 17:03:40 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -190,7 +190,7 @@ with col1:
     color="TOKEN",
     title="Daily Hyperliquid TVL by Token"
     )
-    fig_stacked.update_layout(barmode="stack", yaxis_title="USD")
+    fig_stacked.update_layout(barmode="stack", yaxis_title="USD"legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
     st.plotly_chart(fig_stacked, use_container_width=True)
 
 with col2:
