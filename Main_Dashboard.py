bcb3f0299ae41d6d99a1891008206527ecb57438
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-03 16:35:43 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -77,7 +77,7 @@ with col1:
     timeframe = st.selectbox("Select Time Frame", ["week", "month", "day"])
 
 with col2:
-    start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
+    start_date = st.date_input("Start Date", value=pd.to_datetime("2023-02-26"))
 
 with col3:
     end_date = st.date_input("End Date", value=pd.to_datetime("2025-08-31"))
@@ -90,3 +90,107 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+# --- Row 1 ---------------------------------------------------------------------------------------------------------------
+@st.cache_data
+def load_time_series_data(timeframe, start_date, end_date):
+    start_str = start_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
+
+    query = f"""
+    WITH overview as (
+WITH axelar_gmp AS (
+  
+  SELECT  
+    created_at,
+    LOWER(data:call.chain::STRING) AS source_chain,
+    LOWER(data:call.returnValues.destinationChain::STRING) AS destination_chain,
+    data:call.transaction.from::STRING AS user,
+
+    CASE 
+      WHEN IS_ARRAY(data:amount) OR IS_OBJECT(data:amount) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:amount::STRING)
+      ELSE NULL
+    END AS amount,
+
+    CASE 
+      WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+      ELSE NULL
+    END AS amount_usd,
+
+    COALESCE(
+      CASE 
+        WHEN IS_ARRAY(data:gas:gas_used_amount) OR IS_OBJECT(data:gas:gas_used_amount) 
+          OR IS_ARRAY(data:gas_price_rate:source_token.token_price.usd) OR IS_OBJECT(data:gas_price_rate:source_token.token_price.usd) 
+        THEN NULL
+        WHEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) IS NOT NULL 
+          AND TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING) IS NOT NULL 
+        THEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) * TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING)
+        ELSE NULL
+      END,
+      CASE 
+        WHEN IS_ARRAY(data:fees:express_fee_usd) OR IS_OBJECT(data:fees:express_fee_usd) THEN NULL
+        WHEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING)
+        ELSE NULL
+      END
+    ) AS fee,
+
+    id, 
+    'GMP' AS "Service", 
+    data:symbol::STRING AS raw_asset
+
+  FROM axelar.axelscan.fact_gmp 
+  WHERE status = 'executed'
+    AND simplified_status = 'received'
+    )
+
+SELECT created_at, id, user, source_chain, destination_chain,
+     "Service", amount, amount_usd, fee
+
+FROM axelar_gmp)
+
+select 
+date_trunc('{timeframe}',created_at) as "Date",
+count(distinct id) as "Total Transactions",
+count(distinct user) as "Unique Users",
+round(sum(amount_usd)) as "Total Volume"
+from overview
+where created_at::date>='{start_str}' and created_at::date<='{end_str}'
+group by 1
+order by 1
+    """
+
+    return pd.read_sql(query, conn)
+
+# --- Load Data ----------------------------------------------------------------------------------------------------
+df_ts = load_time_series_data(timeframe, start_date, end_date)
+# --- Row 2 charts -------------------------------------------------------------------------------------------------
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    fig1 = px.area(df_ts, x="Date", y="Total Volume", title="Volume Over Time ($USD)")
+    fig1.update_layout(
+        xaxis_title=" ",
+        yaxis_title="$USD",
+        template="plotly_white"
+    )
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    fig2 = px.area(df_ts, x="Date", y="Total Volume", title="Volume Over Time ($USD)")
+    fig2.update_layout(
+        xaxis_title=" ",
+        yaxis_title="$USD",
+        template="plotly_white"
+    )
+    st.plotly_chart(fig2, use_container_width=True)
+
+with col3:
+    fig3 = px.area(df_ts, x="Date", y="Total Volume", title="Volume Over Time ($USD)")
+    fig3.update_layout(
+        xaxis_title=" ",
+        yaxis_title="$USD",
+        template="plotly_white"
+    )
+    st.plotly_chart(fig3, use_container_width=True)
