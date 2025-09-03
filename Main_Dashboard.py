361da7da290c37f0653833ed7e5fb7c326b05edc
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-04 00:03:06 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -912,3 +912,22 @@ with col2:
     st.plotly_chart(bar_fig_median, use_container_width=True)    
 with col3:
     st.plotly_chart(fig_donut_volume, use_container_width=True)
+
+
+# --- Reference Info ------------------------------------------------------------------------------------------------------------------------
+
+st.markdown(
+    """
+    <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
+        <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 10px;">
+            <img src="https://cdn-icons-png.flaticon.com/512/3178/3178287.png" alt="Reference" style="width:20px; height:20px;">
+            <span>Dashboard Reference: <a href="https://flipsidecrypto.xyz/pine/hyperliquid-bridge-metrics-lxNyGO" target="_blank">https://flipsidecrypto.xyz/pine/hyperliquid-bridge-metrics-lxNyGO/</a></span>
+        </div>
+        <div style="display: flex; align-items: center; gap: 10px;">
+            <img src="https://pbs.twimg.com/profile_images/1856738793325268992/OouKI10c_400x400.jpg" alt="Flipside" style="width:25px; height:25px; border-radius: 50%;">
+            <span>Data Powered by: <a href="https://flipsidecrypto.xyz/home/" target="_blank">Flipside</a></span>
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
